d5a2e1ea12304a285b5e2ced51d97e5527b557d3
sandisile-12t <109766668+sandisile-12t@users.noreply.github.com>
2026-05-15 20:49:12 +0200

    installing node packages

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import {AppRegistry} from 'react-native';
-import App from './App';
+import App from './App';   // this points to your App.js
 import {name as appName} from './app.json';
 
+// Register the root component
 AppRegistry.registerComponent(appName, () => App);
